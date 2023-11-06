4a6d778050d997f5e9627f9fa3dea0760a843922
Artiom Mezheynikov <artiom.mezheynikov.work@gmail.com>
2023-11-06 15:48:28 +0300

    add css/js  slider in section character-samples

---
+++ js/slider-character-samples.js
@@ -0,0 +1,39 @@
+const sliderImages = document.querySelectorAll('.slider-character-samples__image'),
+	sliderLine = document.querySelector('.slider-character-samples__line'),
+	sliderBtnNext = document.querySelector('.slider-character-samples__btn-next'),
+	sliderBtnPrev = document.querySelector('.slider-character-samples__btn-prev');
+
+let sliderCount = 0,
+	sliderWidth;
+
+window.addEventListener('resize', showSlideWidth);
+sliderBtnPrev.addEventListener('click', switchPrevSlide);
+sliderBtnNext.addEventListener('click', switchNextSlide);
+
+function showSlideWidth() {
+	sliderWidth = document.querySelector('.slider-character-samples').offsetWidth;
+	sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
+	sliderImages.forEach(image => image.style.width = sliderWidth + 'px');
+
+	createRollSlider();
+}
+showSlideWidth();
+
+function switchNextSlide() {
+	sliderCount++;
+	if (sliderCount >= sliderImages.length) sliderCount = 0;
+	createRollSlider();
+}
+
+function switchPrevSlide() {
+	sliderCount--;
+	if (sliderCount < 0) sliderCount = sliderImages.length - 1;
+	createRollSlider();
+}
+
+function createRollSlider() {
+	sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+setInterval(() => {
+	switchNextSlide()
+}, 5000);
